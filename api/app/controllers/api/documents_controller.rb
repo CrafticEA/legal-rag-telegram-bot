77b128c428a1222ab0ed28f17ea3ce7b86593f20
Sergio <seregaant71@gmail.com>
2026-03-13 20:16:09 +0300

    add test, add new routes

---
+++ api/app/controllers/api/documents_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Api
+  class DocumentsController < ApplicationController
+    before_action :set_case
+
+    # POST api/cases/:case_id/documents (multipart/form-data, field: file)
+    def create
+      @document = @case.documents.build(document_params)
+      if @document.save
+        render json: { doc_id: document_id(@document), ok: true }, status: :created
+      else
+        render json: { errors: @document.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE api/cases/:case_id/documents/:id?chat_id=123456789
+    def destroy
+      @document = @case.documents.find(params[:id])
+      doc_id = document_id(@document)
+      @document.destroy
+      @case.update!(status: 'DIRTY')
+      render json: { ok: true, doc_id: doc_id, case_status: @case.status }
+    end
+
+    private
+
+    def set_case
+      @case = Case.where(chat_id: params[:chat_id]).find(params[:case_id])
+    end
+
+    def document_params
+      params.permit(:file).slice(:file).to_h
+    end
+
+    def document_id(document)
+      document.doc_id
+    end
+  end
+end
--- api/app/views/api/cases/status.json.jbuilder
+++ api/app/views/api/cases/status.json.jbuilder
@@ -1,2 +1,3 @@
-json.id @case.id
+json.case_id "case_#{@case.id}"
 json.status @case.status
+json.progress @case.progress.to_i
